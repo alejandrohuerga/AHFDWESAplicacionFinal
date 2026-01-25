46bbb4b81f70ca39a9ea72b9e2768646cf231d21
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-01-25 22:12:32 +0100

    fix: :bug: Corrección de bug en index.php
    se ha corregido un bug producido en el index.php

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
  * @author Alejandro De la Huerga
  * @since 16/12/2025
  */
-   // Iniciamos la sesión
-   session_start();
+   
 
    // Cargamos los archivos de configuración
    require_once 'config/confAPP.php'; // Incluir estos dos y en estos dos incluir el modelo.
    require_once 'config/confDBPDO.php';
 
+   // Iniciamos la sesión
+   session_start();
+   
    // Si no esta la página en curso con la sesión la creamos con inicio público.
    if(!isset($_SESSION['paginaEnCurso'])){
       $_SESSION['paginaEnCurso'] = 'inicioPublico';
